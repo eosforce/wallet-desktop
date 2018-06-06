c2b623e263a81ea942e613f8fb4cd9f94604b5e0
eosforce <eosfio2018@gmail.com>
2018-06-06 10:56:00 +0800

    change bp

--- src/renderer/services/Eos.js
+++ src/renderer/services/Eos.js
@@ -1,7 +1,16 @@
-import Eos from 'eosjs'
+import Eos from 'eosjs';
 
-import { NODE_API_URL } from '@/constants/config.constants'
-import { toAsset, toBigNumber, getTimeStamp, isZero, calcTotalAmount, clacReward, handleApiError, clacAverage } from '@/utils/util'
+import { NODE_API_URL } from '@/constants/config.constants';
+import {
+  toAsset,
+  toBigNumber,
+  getTimeStamp,
+  isZero,
+  calcTotalAmount,
+  clacReward,
+  handleApiError,
+  calcBpVoteage,
+} from '@/utils/util';
 
 export const getNodeList = () => {
   return Promise.resolve([
@@ -37,7 +46,7 @@ export const getNodeList = () => {
       port_ssl: '',
       port_p2p: '9876',
     },
-  ])
+  ]);
   // return fetch(NODE_API_URL, {
   //   headers: {
   //     Accept: 'application/vnd.github.raw+json',
@@ -50,29 +59,29 @@ export const getNodeList = () => {
   //   .catch(err => {
   //     return Promise.reject(new Error('获取节点列表失败！'))
   //   })
-}
+};
 
 export const getNodeInfo = httpEndpoint => {
-  return Eos.Localnet({ httpEndpoint }).getInfo({})
-}
+  return Eos.Localnet({ httpEndpoint }).getInfo({});
+};
 
 export const createLocalNet = ({ chain_id, http_endpoint }) => config => {
-  return Eos.Localnet({ httpEndpoint, chainId: chain_id, ...config })
-}
+  return Eos.Localnet({ httpEndpoint, chainId: chain_id, ...config });
+};
 
 // 根据公钥获取用户名数组
 export const getAccounts = httpEndpoint => publicKey => {
   return Eos.Localnet({ httpEndpoint })
     .getKeyAccounts({ public_key: publicKey })
     .then(result => {
-      return result.account_names || []
-    })
-}
+      return result.account_names || [];
+    });
+};
 
 // 获取交易记录
 export const getTransferRecord = httpEndpoint => ({ accountName, pos, offset }) => {
-  return Eos.Localnet({ httpEndpoint }).getActions({ account_name: accountName, pos: pos, offset: offset, limit: 100 })
-}
+  return Eos.Localnet({ httpEndpoint }).getActions({ account_name: accountName, pos: pos, offset: offset, limit: 100 });
+};
 
 // 从节点创建用户
 export const newAccountFromNode = httpEndpoint => ({ accountName, publicKey }) => {
@@ -85,9 +94,9 @@ export const newAccountFromNode = httpEndpoint => ({ accountName, publicKey }) =
       },
     })
     .catch(err => {
-      return handleApiError(err)
-    })
-}
+      return handleApiError(err);
+    });
+};
 
 // 创建用户
 export const newAccount = config => ({ creator, accountName, publicKey }) => {
@@ -99,9 +108,9 @@ export const newAccount = config => ({ creator, accountName, publicKey }) => {
       active: publicKey,
     })
     .catch(err => {
-      return handleApiError(err)
-    })
-}
+      return handleApiError(err);
+    });
+};
 
 // 获取指定账户可用余额
 export const getAvailable = httpEndpoint => accountName => {
@@ -115,14 +124,14 @@ export const getAvailable = httpEndpoint => accountName => {
       json: true,
     })
     .then(result => {
-      const account = result.rows.find(acc => acc.name === accountName)
+      const account = result.rows.find(acc => acc.name === accountName);
       if (account) {
-        return toBigNumber(account.available)
+        return toBigNumber(account.available);
       } else {
-        return toBigNumber(0)
+        return toBigNumber(0);
       }
-    })
-}
+    });
+};
 
 // 获取 bp 表
 export const getBpsTable = httpEndpoint => () => {
@@ -133,43 +142,46 @@ export const getBpsTable = httpEndpoint => () => {
       return data.sort((val1, val2) => {
         return toBigNumber(val2.total_staked)
           .minus(toBigNumber(val1.total_staked))
-          .toNumber()
-      })
-    })
-}
+          .toNumber();
+      });
+    });
+};
 
 // 获取 vote 表
 export const getVotesTable = httpEndpoint => accountName => {
   return Eos.Localnet({ httpEndpoint, chain_id: 'dc85ad2842e7d62c699d952b26fa7abe11fe90c00004b9d6b0eac55a44e3bbe1' })
     .getTableRows({ scope: accountName, code: 'eosio', table: 'votes', json: true, limit: 1000000 })
-    .then(data => data.rows)
-}
+    .then(data => data.rows);
+};
 
 // 根据 bp 和 vote 得到分红表，返回一个对象
 export const getRewardsAndBpsTable = httpEndpoint => async (votesTable, accountName) => {
-  const bpsTable = await getBpsTable(httpEndpoint)()
-  const bpsHaveVoteTable = []
-  const bpsNoVoteTable = []
-  const rewardsTable = []
-  let bpInfo
+  const bpsTable = await getBpsTable(httpEndpoint)();
+  const bpsHaveVoteTable = [];
+  const bpsNoVoteTable = [];
+  const rewardsTable = [];
+  let bpInfo;
   for (const [index, bpRow] of bpsTable.entries()) {
-    bpRow.order = index + 1
-    const vote = votesTable.find(row => row.bpname === bpRow.name)
+    bpRow.order = index + 1;
+    const vote = votesTable.find(row => row.bpname === bpRow.name);
     if (bpRow.name === accountName) {
       bpInfo = {
         bpname: bpRow,
         average: toAsset(toBigNumber(bpRow.total_voteage) / toBigNumber(bpRow.total_staked)),
         ...bpRow,
-      }
+      };
     }
-    const { rewards_pool, total_voteage, total_staked, voteage_update_time } = bpRow
-    const average = clacAverage(total_voteage, total_staked, voteage_update_time)
 
-    bpRow.average = average
+    const { rewards_pool, total_voteage, total_staked, voteage_update_time } = bpRow;
+    bpRow.bp_voteage = calcBpVoteage(total_voteage, total_staked, voteage_update_time);
     if (vote) {
-      const { bpname, staked, stake_time, unstaking } = vote
-      const reward = toAsset(clacReward(staked, stake_time, total_voteage, rewards_pool, total_staked))
-
+      const { bpname, staked, stake_time, unstaking, voteage } = vote;
+      bpRow.me_voteage = calcBpVoteage(voteage, total_staked, voteage_update_time);
+      const reward = toAsset(clacReward(staked, bpRow.me_voteage, total_staked, bpRow.bp_voteage, rewards_pool));
+      const average = toBigNumber(total_voteage)
+        .dividedBy(10000)
+        .dividedBy(toBigNumber(total_staked))
+        .toString();
       const rewardRow = {
         bpname,
         staked,
@@ -180,37 +192,38 @@ export const getRewardsAndBpsTable = httpEndpoint => async (votesTable, accountN
         total_staked,
         reward,
         average,
-      }
-      rewardsTable.push({ ...rewardRow })
-      bpRow.vote = { ...rewardRow }
-      bpsHaveVoteTable.push(bpRow)
+      };
+      rewardsTable.push({ ...rewardRow });
+
+      bpRow.vote = { ...rewardRow };
+      bpsHaveVoteTable.push(bpRow);
     } else {
-      bpsNoVoteTable.push(bpRow)
+      bpsNoVoteTable.push(bpRow);
     }
   }
 
   bpsHaveVoteTable.sort((val1, val2) => {
     return toBigNumber(val2.vote.staked)
       .minus(toBigNumber(val1.vote.staked))
-      .toNumber()
-  })
+      .toNumber();
+  });
 
   return {
     rewardsTable,
     bpsTable: bpsHaveVoteTable.concat(bpsNoVoteTable),
     bpInfo,
-  }
-}
+  };
+};
 
 export const getAccountInfo = httpEndpoint => async accountName => {
-  const votesTable = await getVotesTable(httpEndpoint)(accountName)
-  const { rewardsTable, bpsTable, bpInfo } = await getRewardsAndBpsTable(httpEndpoint)(votesTable, accountName)
+  const votesTable = await getVotesTable(httpEndpoint)(accountName);
+  const { rewardsTable, bpsTable, bpInfo } = await getRewardsAndBpsTable(httpEndpoint)(votesTable, accountName);
 
-  const available = await getAvailable(httpEndpoint)(accountName)
-  const stakedTotal = calcTotalAmount(votesTable, 'staked')
-  const unstakingTotal = calcTotalAmount(votesTable, 'unstaking')
-  const rewardTotal = calcTotalAmount(rewardsTable, 'reward')
-  const assetTotal = calcTotalAmount([available, stakedTotal, unstakingTotal, rewardTotal])
+  const available = await getAvailable(httpEndpoint)(accountName);
+  const stakedTotal = calcTotalAmount(votesTable, 'staked');
+  const unstakingTotal = calcTotalAmount(votesTable, 'unstaking');
+  const rewardTotal = calcTotalAmount(rewardsTable, 'reward');
+  const assetTotal = calcTotalAmount([available, stakedTotal, unstakingTotal, rewardTotal]);
 
   const info = {
     assetTotal: toAsset(assetTotal), // 总资产
@@ -218,54 +231,54 @@ export const getAccountInfo = httpEndpoint => async accountName => {
     stakedTotal: toAsset(stakedTotal), // 总投票金额
     unstakingTotal: toAsset(unstakingTotal), // 总赎回金额
     rewardTotal: toAsset(rewardTotal), // 总待领取分红
-  }
+  };
 
   if (bpInfo) {
-    info.bpInfo = bpInfo
+    info.bpInfo = bpInfo;
   }
 
   return {
     info,
     bpsTable,
-  }
-}
+  };
+};
 
 export const transfer = config => {
   return ({ from, to, amount } = {}) => {
     return Eos.Localnet(config)
       .transfer({ from, to, quantity: toAsset(amount), memo: '' })
       .catch(err => {
-        return handleApiError(err)
-      })
-  }
-}
+        return handleApiError(err);
+      });
+  };
+};
 
 export const vote = config => {
   return ({ voter, bpname, amount } = {}) => {
     return Eos.Localnet(config)
       .vote({ voter, bpname, change: toAsset(amount) })
       .catch(err => {
-        return handleApiError(err)
-      })
-  }
-}
+        return handleApiError(err);
+      });
+  };
+};
 
 export const unfreeze = config => {
   return ({ voter, bpname } = {}) => {
     return Eos.Localnet(config)
       .unfreeze({ voter, bpname })
       .catch(err => {
-        return handleApiError(err)
-      })
-  }
-}
+        return handleApiError(err);
+      });
+  };
+};
 
 export const claim = config => {
   return ({ voter, bpname } = {}) => {
     return Eos.Localnet(config)
       .claim({ voter, bpname })
       .catch(err => {
-        return handleApiError(err)
-      })
-  }
-}
+        return handleApiError(err);
+      });
+  };
+};
