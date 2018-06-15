9e66c000196269d067b72b4851a7265ddcddc5af
eosforce <eosfio2018@gmail.com>
2018-06-15 14:17:11 +0800

    update

--- src/renderer/views/account/AccountDetail.vue
+++ src/renderer/views/account/AccountDetail.vue
@@ -11,7 +11,7 @@
         <a class="min-img">
           <img :src="require(`@/assets/${tab.img1}`)" class="two hide">
           <img :src="require(`@/assets/${tab.img2}`)" class="one">
-          {{tab.tabName}}<span v-show="tab.tabKey === 'BpList'">(第{{version}}届)</span>
+          {{tab.tabName}}<span v-show="tab.tabKey === 'BpList'">(第 {{version}} 届)</span>
         </a>
       </div>
       <span class="refresh fr el-icon-refresh" @click="refreshList()"></span>
@@ -41,7 +41,7 @@ export default {
         { tabName: `超级节点`, tabKey: 'BpList', img1: 'node.png', img2: 'node_w.png' },
         { tabName: '候选节点', tabKey: 'AlternateNode', img1: 'laternode.png', img2: 'laternode_w.png' },
         { tabName: '我的投票', tabKey: 'Myvote', img1: 'vote.png', img2: 'vote_w.png' },
-        { tabName: '我的 Token', tabKey: 'TokenList', img1: 'vote.png', img2: 'vote_w.png' },
+        { tabName: '我的 Token', tabKey: 'TokenList', img1: 'token.png', img2: 'token_w.png' },
         { tabName: '交易记录', tabKey: 'TransferRecord', img1: 'exchange.png', img2: 'exchange_w.png' },
         { tabName: '资产说明', tabKey: 'RateInstructions', img1: 'assets.png', img2: 'assets_w.png' },
       ],
