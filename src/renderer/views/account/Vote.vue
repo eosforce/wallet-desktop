141202267310565b9e6552b8d59cec076f155db6
eosforce <eosfio2018@gmail.com>
2018-06-22 12:54:50 +0800

    更新手续费

--- src/renderer/views/account/Vote.vue
+++ src/renderer/views/account/Vote.vue
@@ -65,7 +65,7 @@
         </div>
         <div class="row">
           <div class="row__title">手续费</div>
-          <div class="row__content">{{app.fee}}</div>
+          <div class="row__content">0.05 EOS</div>
         </div>
         <div class="row">
           <div class="row__title">输入密码</div>
