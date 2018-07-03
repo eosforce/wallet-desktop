22e80e127026222705fed69f2aa843ce66189d0a
eosforce <eosfio2018@gmail.com>
2018-07-03 16:37:28 +0800

    update

--- src/renderer/constants/config.constants.js
+++ src/renderer/constants/config.constants.js
@@ -4,8 +4,7 @@ export const NODE_LIST_KEY = 'storage://node/nodelist';
 
 export const CHAIN_NET_KEY = 'storage://currentChainNet';
 
-// export const NODE_API_URL = 'https://raw.githubusercontent.com/eosforce/testnet-v0.5/master/node-list/nodes.json';
-export const NODE_API_URL = 'https://raw.githubusercontent.com/eosforce/testnet-main/master/node-list/main-net.json';
+export const NODE_API_URL = 'https://updatewallet.oss-cn-hangzhou.aliyuncs.com/eosforce/main-net.json';
 // export const NODE_TEST_NET_URL = 'https://raw.githubusercontent.com/eosforce/testnet-main/master/node-list/test-list.json';
 
 export const CHAIN_NETS = {
