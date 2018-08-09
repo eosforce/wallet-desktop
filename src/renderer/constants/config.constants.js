c38afe4a6d1a0a4e662f65d4442158892cbe6568
bandon li <begininit@163.com>
2018-08-09 13:57:00 +0800

    wallet for test node

--- src/renderer/constants/config.constants.js
+++ src/renderer/constants/config.constants.js
@@ -4,7 +4,7 @@ export const NODE_LIST_KEY = 'storage://node/nodelist';
 
 export const CHAIN_NET_KEY = 'storage://currentChainNet';
 
-export const NODE_API_URL = 'https://updatewallet.oss-cn-hangzhou.aliyuncs.com/eosforce/main-net.json';
+export const NODE_API_URL = 'https://updatewallet.oss-cn-hangzhou.aliyuncs.com/eosforce/main-net-test.json';
 // export const NODE_TEST_NET_URL = 'https://raw.githubusercontent.com/eosforce/testnet-main/master/node-list/test-list.json';
 
 export const CHAIN_NETS = {
