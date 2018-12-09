cd4000ea73d4e98c23b4a6e25680c1f95e97b89c
bandon li <begininit@163.com>
2018-12-09 17:11:46 +0800

    develop for test node

--- src/renderer/services/Eos.js
+++ src/renderer/services/Eos.js
@@ -38,12 +38,12 @@ export const getNodeList = () => {
   };
   return fetch(map[Store.state.app.chainNet]).then(async res => {
     let data = await res.json();
-    //trans_main
-    // data.nodes.forEach(item => {
-    //   item.node_addr = '47.99.167.137';
-    //   item.port_http = '19001';
-    //   item.port_ssl = '';
-    // });
+    trans_main
+    data.nodes.forEach(item => {
+      item.node_addr = '47.99.138.131';
+      item.port_http = '19000';
+      item.port_ssl = '';
+    });
     return data;
   });
 };
