929b85f551d9ddd6463f619b610c361a11b4e38a
bandon li <begininit@163.com>
2019-01-27 01:48:07 +0800

    create wallet split to 2 function: create with private key and create with random key

--- src/renderer/views/wallet/CreateKey.vue
+++ src/renderer/views/wallet/CreateKey.vue
@@ -2,7 +2,7 @@
 <div class="cover-page">
 
   <div class="cover-page__content">
-      <CreateWalletForm v-bind:with_close="false" v-bind:title="$t('创建秘钥')" v-bind:with_title="false" v-bind:with_random="true" v-bind:margin_top="2"></CreateWalletForm>
+      <CreateWalletForm v-bind:with_close="false" v-bind:title="$t('创建私钥')" v-bind:with_title="false" v-bind:with_random="true" v-bind:margin_top="2"></CreateWalletForm>
   </div>
 
   <a class="modal-close is-large cover-page-close" @click="close"></a>
